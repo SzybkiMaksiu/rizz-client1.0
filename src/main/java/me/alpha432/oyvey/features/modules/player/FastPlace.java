02ad5a404a0ac06e3b78aadc555cd19c7345c47a
SzybkiMaksiu <noobmarszalkowski@gmail.com>
2025-12-22 19:14:12 +0100

    FastPlace.java

--- src/main/java/me/alpha432/oyvey/features/modules/player/FastPlace.java
+++ src/main/java/me/alpha432/oyvey/features/modules/player/FastPlace.java
@@ -5,14 +5,16 @@ import net.minecraft.world.item.Items;
 
 public class FastPlace extends Module {
     public FastPlace() {
-        super("FastPlace", "Makes you throw exp faster", Category.PLAYER);
+        // Zaktualizowałem opis, aby pasował do nowych przedmiotów
+        super("FastPlace", "Makes you place cobwebs and planks faster", Category.PLAYER);
     }
 
     @Override
     public void onTick() {
         if (nullCheck()) return;
 
-        if (mc.player.isHolding(Items.EXPERIENCE_BOTTLE)) {
+        // Sprawdza, czy gracz trzyma pajęczynę LUB dębowe deski
+        if (mc.player.isHolding(Items.COBWEB) || mc.player.isHolding(Items.OAK_PLANKS)) {
             mc.rightClickDelay = 0;
         }
     }
